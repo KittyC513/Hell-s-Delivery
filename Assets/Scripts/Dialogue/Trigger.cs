764e58f542f4725892df8cc2b2f511f2adc39f32
kateewhy <138513011+kateewhy001@users.noreply.github.com>
2023-10-16 22:33:16 -0400

    Updated dialogue hubstart scene

--- Assets/Scripts/Dialogue/Trigger.cs
+++ Assets/Scripts/Dialogue/Trigger.cs
@@ -25,5 +25,9 @@ public class Trigger : MonoBehaviour
         {
             dR.StartDialogue("HubStart");
         }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
