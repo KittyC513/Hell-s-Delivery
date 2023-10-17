e6ba4672e61e099be4344e51493a3628238aa47b
KittyC513 <82276776+KittyC513@users.noreply.github.com>
2023-10-17 13:50:09 -0400

    Update Trigger Dialogue

--- Assets/Scripts/Dialogue/Trigger.cs
+++ Assets/Scripts/Dialogue/Trigger.cs
@@ -6,11 +6,18 @@ using Yarn.Unity;
 public class Trigger : MonoBehaviour
 {
     public DialogueRunner dR;
+    [SerializeField]
+    private LayerMask layer1;
+    [SerializeField]
+    private LayerMask layer2;
+
+    public bool isTriggered, canDestroy;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        layer1 = LayerMask.NameToLayer("P1Collider");
+        layer2 = LayerMask.NameToLayer("P2Collider");
     }
 
     // Update is called once per frame
@@ -21,13 +28,27 @@ public class Trigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if( other.gameObject.layer ==layer1)
         {
-            dR.StartDialogue("HubStart");
+            Debug.Log("Other gameobject1 = " + other.gameObject);
+            //dR.StartDialogue("HubStart");
+            isTriggered = true;
         }
-        else
+
+        if (other.gameObject.layer == layer2 && isTriggered)
         {
-            Destroy(gameObject);
+            Debug.Log("Other gameobject2 = " + other.gameObject);
+            dR.StartDialogue("HubStart");
+            canDestroy = true;
+
+        }
+    }
+
+    public void DestroyThis()
+    {
+        if (canDestroy)
+        {
+            Destroy(this.gameObject);
         }
     }
 }
