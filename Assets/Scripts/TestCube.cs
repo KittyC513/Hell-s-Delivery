065703bf6b19ff2068b46187bec3dd610a9c2d18
kateewhy <138513011+kateewhy001@users.noreply.github.com>
2023-10-18 12:36:46 -0400

    Updated HubEnd Dialogue

--- Assets/Scripts/TestCube.cs
+++ Assets/Scripts/TestCube.cs
@@ -92,10 +92,13 @@ public class TestCube : MonoBehaviour
     string curSceneName;
     string scene1 = "HubStart";
     string scene2 = "PrototypeLevel";
+    string scene3 = "HubEnd";
     [SerializeField]
     bool withinDialogueRange;
     [SerializeField]
     bool conversationStart;
+    bool hubStart;
+    bool hubEnd;
     [SerializeField]
     public bool isFreeze;
     [SerializeField]
@@ -193,7 +196,7 @@ public class TestCube : MonoBehaviour
 
     private void Move()
     {
-        if (curSceneName == scene1)
+        if (curSceneName == scene1 || curSceneName == scene3)
         {
             //Debug.Log("Camera: " + curSceneName == scene1);
             playerCamera.enabled = false;
@@ -352,7 +355,14 @@ public class TestCube : MonoBehaviour
         if (other.gameObject.tag == "NPC1")
         {
             withinDialogueRange = true;
+            hubStart = true;
                     
+        }
+        else if (other.gameObject.tag == "NPC2")
+        {
+            Debug.Log("in range");
+            withinDialogueRange = true;
+            hubEnd = true;
         }
     }
 
@@ -361,15 +371,22 @@ public class TestCube : MonoBehaviour
     {
         if (withinDialogueRange)
         {
-            if (!conversationStart)
+            if (!conversationStart && hubStart == true)
             {
                 dR.StartDialogue("BoomerQuest");
                 conversationStart = true;
                 lineView = FindObjectOfType<LineView>();
-                
+                withinDialogueRange = false;
+
             }
-
-
+            
+            if(!conversationStart && hubEnd == true)
+            {
+                dR.StartDialogue("HubEnd");
+                conversationStart = true;
+                lineView = FindObjectOfType<LineView>();
+                withinDialogueRange = false;
+            }
         }
     }
 
