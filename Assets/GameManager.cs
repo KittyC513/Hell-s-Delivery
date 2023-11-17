135fe6bc19ad210c386f969d6772a622b05f841f
KittyC513 <shuichunchenkitty@gmail.com>
2023-11-16 22:21:51 -0500

    Update Title scene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -16,13 +16,30 @@ public class GameManager : MonoBehaviour
     private GameObject player1;
     [SerializeField]
     private GameObject player2;
+
+    [SerializeField]
+    private GameObject character1;
+    [SerializeField]
+    private GameObject character2;
+    [SerializeField]
+    private bool readyToStart;
+    [SerializeField]
+    private Animator p1Ani;
+    [SerializeField]
+    private Animator p2Ani;
+    [SerializeField]
+    private Animator titleAni;
+    [SerializeField]
+    private float destroyTime;
+
     [SerializeField]
     public bool sceneChanged;
 
     private void Start()
     {
         sceneChanged = false;
-
+        character1.SetActive(false);
+        character2.SetActive(false);
     }
 
     private void Update()
@@ -43,15 +60,30 @@ public class GameManager : MonoBehaviour
             {
                 player1 = obj;
                 p1 = obj.GetComponent<TestCube>();
+                character1.SetActive(true);
+
+
             }
 
             if (obj.layer == layerToFind2 && p2 == null)
             {
                 player2 = obj;
                 p2 = obj.GetComponent<TestCube>();
+                character2.SetActive(true);
             }
         }
+
+        //two players join the game, it loads to the Title Scene
+        if(p1 != null && p2 != null)
+        {
+            p1Ani.SetBool("GameStart", true);
+            p2Ani.SetBool("GameStart", true);
+            titleAni.SetBool("GameStart", true);
+            StartCoroutine(DestroyAfterDelay());
+        }
     }
+
+ 
     public void FreezePlayer()
     {
         if (p1 != null && p2 != null)
@@ -109,6 +141,16 @@ public class GameManager : MonoBehaviour
         {
             Destroy(player2);
         }
+    }
+
+    IEnumerator DestroyAfterDelay()
+    {
+        // Wait for the specified time
+        yield return new WaitForSeconds(destroyTime);
+
+        // Destroy the GameObject this script is attached to
+        Destroy(character1);
+        Destroy(character2);
     }
 
 }
