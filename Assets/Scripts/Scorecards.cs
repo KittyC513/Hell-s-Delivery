151edb05e3743695f7bf49b4a7bd1dcd87211835
Chummy-Guy <78655854+Chummy-Guy@users.noreply.github.com>
2024-01-26 20:20:48 -0500

    Scorecards update

--- Assets/Scripts/Scorecards.cs
+++ Assets/Scripts/Scorecards.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Scorecards : MonoBehaviour
 {
+    [Header ("Data")]
     [SerializeField]
     private float player1Score;
     [SerializeField]
     private float player2Score;
     [SerializeField]
     public LevelData lvlData;
+    [SerializeField]
+    public PlayerScoreData playerScoreData;
+    [SerializeField] private int thumbsUpBonus = 50;
+    [SerializeField] private int badgeBonus = 50;
 
     [SerializeField] private AK.Wwise.Event scoreCardSequence;
 
@@ -17,18 +22,18 @@ public class Scorecards : MonoBehaviour
     [SerializeField] private GameObject happySticker;
     [SerializeField] private GameObject neutralSticker;
     [SerializeField] private GameObject sadSticker;
-    [SerializeField] private GameObject aGrade;
-    [SerializeField] private GameObject bGrade;
-    [SerializeField] private GameObject cGrade;
-    [SerializeField] private GameObject dGrade;
+    //[SerializeField] private GameObject aGrade;
+    //[SerializeField] private GameObject bGrade;
+    //[SerializeField] private GameObject cGrade;
+    //[SerializeField] private GameObject dGrade;
 
     [Header ("Sticker Spots")]
     [SerializeField] private Transform p1DeathSpot;
     [SerializeField] private Transform p2DeathSpot;
     [SerializeField] private Transform p1PackageSpot;
     [SerializeField] private Transform p2PackageSpot;
-    [SerializeField] private Transform p1CompletionSpot;
-    [SerializeField] private Transform p2CompletionSpot;
+    //[SerializeField] private Transform p1CompletionSpot;
+    //[SerializeField] private Transform p2CompletionSpot;
 
     [SerializeField] private Transform p1TotalSpot;
     [SerializeField] private Transform p2TotalSpot;
@@ -58,21 +63,64 @@ public class Scorecards : MonoBehaviour
     private IEnumerator AnimationCycle()
     {
         yield return new WaitForSeconds(0.85f);
-        
-        p1Stickers[0] = CheckScore(lvlData.p1Deaths, p1DeathSpot.position, categories.deaths);
-        p2Stickers[0] = CheckScore(lvlData.p2Deaths, p2DeathSpot.position, categories.deaths);
+
+        //p1Stickers[0] = CheckScore(lvlData.p1Deaths, p1DeathSpot.position, categories.deaths);
+        //p2Stickers[0] = CheckScore(lvlData.p2Deaths, p2DeathSpot.position, categories.deaths);
+        CompareScore(lvlData.p1Deaths, lvlData.p2Deaths, categories.deaths, p1DeathSpot.position, p2DeathSpot.position);
         yield return new WaitForSeconds(0.44f);
-        scoreCardSequence.Post(this.gameObject);
+        //scoreCardSequence.Post(this.gameObject);
         yield return new WaitForSeconds(0.31f);
-        p1Stickers[1] = CheckScore(lvlData.p1Deliver / (lvlData.p1Deliver + lvlData.p2Deliver), p1PackageSpot.position, categories.package);
-        p2Stickers[1] = CheckScore(lvlData.p2Deliver / (lvlData.p1Deliver + lvlData.p2Deliver), p2PackageSpot.position, categories.package);
+        //p1Stickers[1] = CheckScore(lvlData.p1Deliver / (lvlData.p1Deliver + lvlData.p2Deliver), p1PackageSpot.position, categories.package);
+        //p2Stickers[1] = CheckScore(lvlData.p2Deliver / (lvlData.p1Deliver + lvlData.p2Deliver), p2PackageSpot.position, categories.package);
+        CompareScore(lvlData.p1Deliver, lvlData.p2Deliver, categories.deaths, p1PackageSpot.position, p2PackageSpot.position);
         yield return new WaitForSeconds(0.75f);
-        p1Stickers[2] = CheckScore(lvlData.completionTime, p1CompletionSpot.position, categories.completion);
-        p2Stickers[2] = CheckScore(lvlData.completionTime, p2CompletionSpot.position, categories.completion);
+        //p1Stickers[2] = CheckScore(lvlData.completionTime, p1CompletionSpot.position, categories.completion);
+        //p2Stickers[2] = CheckScore(lvlData.completionTime, p2CompletionSpot.position, categories.completion);
+
         yield return new WaitForSeconds(0.75f);
         FinalGrade(p1TotalSpot.position, p1Stickers);
         FinalGrade(p2TotalSpot.position, p2Stickers);
     }
+
+    //put in both players score and determine which player had the higher score
+    //put a thumbs up or down sticker on each player's category
+    //give that player their earned points
+    private void CompareScore(float p1Score, float p2Score, categories category, Vector3 p1StickerPoint, Vector3 p2StickerPoint, bool higherBetter)
+    {
+        GameObject sticker1 = null;
+        GameObject sticker2 = null;
+
+        if (p1Score > p2Score)
+        {
+            //give a thumbs up sticker to player 1 
+            sticker1 = Instantiate(happySticker, p1StickerPoint, Quaternion.identity);
+            sticker1.transform.rotation = RandomRotation();
+            //give 50 points to player 1
+            playerScoreData.p1Overall += 50;
+
+            sticker2 = Instantiate(sadSticker, p2StickerPoint, Quaternion.identity);
+            sticker2.transform.rotation = RandomRotation();
+        }
+        else if (p2Score > p1Score)
+        {
+            //give a thumbs down sticker to player 1
+            sticker1 = Instantiate(sadSticker, p1StickerPoint, Quaternion.identity);
+            sticker1.transform.rotation = RandomRotation();
+            //do not give player 1 any points
+
+            //give a thumbs up sticker to player 2
+            sticker2 = Instantiate(happySticker, p2StickerPoint, Quaternion.identity);
+            sticker2.transform.rotation = RandomRotation();
+            //give player 2 50 points
+            playerScoreData.p2Overall += 50;
+        }
+        else if (p1Score == p2Score)
+        {
+            //maybe give a neutral sticker ONLY if they have the exact same value of deaths or package time
+        }
+    }
+
+    
 
     //each category needs to have it's score checked
     //score values of good and neutral
@@ -260,21 +308,21 @@ public class Scorecards : MonoBehaviour
             }
         }
 
-        if (score == 9)
-        {
-            Instantiate(aGrade, stickerPoint, Quaternion.identity);
-        }
-        else if (score >= 6)
-        {
-            Instantiate(bGrade, stickerPoint, Quaternion.identity);
-        }
-        else if (score >= 4)
-        {
-            Instantiate(cGrade, stickerPoint, Quaternion.identity);
-        }
-        else if (score <= 3)
-        {
-            Instantiate(dGrade, stickerPoint, Quaternion.identity);
-        }
+        //if (score == 9)
+        //{
+        //    Instantiate(aGrade, stickerPoint, Quaternion.identity);
+        //}
+        //else if (score >= 6)
+        //{
+        //    Instantiate(bGrade, stickerPoint, Quaternion.identity);
+        //}
+        //else if (score >= 4)
+        //{
+        //    Instantiate(cGrade, stickerPoint, Quaternion.identity);
+        //}
+        //else if (score <= 3)
+        //{
+        //    Instantiate(dGrade, stickerPoint, Quaternion.identity);
+        //}
     }
 }
