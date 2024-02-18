38efd6faf74e7f0287dd21f12b08a8c15a35b430
KittyC513 <shuichunchenkitty@gmail.com>
2024-02-17 19:47:39 -0500

    Integrated Tutorial

--- Assets/TutorialCamControl.cs
+++ Assets/TutorialCamControl.cs
@@ -19,20 +19,67 @@ public class TutorialCamControl : MonoBehaviour
     [SerializeField]
     private Camera mainCam;
 
-
-
+    [Header("Package")]
     [SerializeField]
     private bool inPackageArea;
     [SerializeField]
+    private int currentPositionIndex1 = 0;
+    [SerializeField]
+    private Transform[] cameraPositions1;
+    [SerializeField]
+    private Camera cam1;
+    [SerializeField]
+    private bool isActivated;
+
+    [Header("Push")]
+    [SerializeField]
+    private bool inPushArea;
+    [SerializeField]
+    private Camera cam2;
+
+    [Header("PressurePlate")]
+    [SerializeField]
     private bool inPressurePlateArea;
+    [SerializeField]
+    private Camera cam3;
+
+    [Header("GoldSummoning")]
+    [SerializeField]
+    private bool inGoldSumoningArea;
+    [SerializeField]
+    private Camera cam4;
+
+    [Header("Checkpoint")]
     [SerializeField]
     private bool inCheckpointArea;
     [SerializeField]
+    private Camera cam5;
+
+    [Header("SummoningCircle")]
+    [SerializeField]
     private bool inSummoningCircleArea;
+    [SerializeField]
+    private Camera cam6;
+
+    [Header("Sabatage")]
+    [SerializeField]
+    private bool inSabptageArea;
+    [SerializeField]
+    private Camera cam7;
+
+    [Header("DualSummoning")]
     [SerializeField]
     private bool inDualSummoningArea;
     [SerializeField]
-    private bool inSabptageArea;
+    private Camera cam8;
+
+    [Header("PlayerSpecific")]
+    [SerializeField]
+    private bool inPlayerSpecificArea;
+    [SerializeField]
+    private Camera cam9;
+
+
 
 
     // Start is called before the first frame update
@@ -53,8 +100,44 @@ public class TutorialCamControl : MonoBehaviour
     {
         if (atStart)
         {
+            GameManager.instance.cam1.SetActive(false);
+            GameManager.instance.cam2.SetActive(false);
+            GameManager.instance.p1.isFreeze = true;
+            GameManager.instance.p2.isFreeze = true;
+            mainCam.gameObject.SetActive(true);
+            cam1.gameObject.SetActive(false);
+            cam2.gameObject.SetActive(false);
+            cam3.gameObject.SetActive(false);
+            cam4.gameObject.SetActive(false);
+            cam5.gameObject.SetActive(false);
+            cam6.gameObject.SetActive(false);
+            cam7.gameObject.SetActive(false);
+            cam8.gameObject.SetActive(false);
+            cam9.gameObject.SetActive(false);
+
             StartCoroutine(MoveToNextCamera());
             atStart = false;
+        }
+
+        if(currentPositionIndex >= cameraPositions.Length - 1)
+        {
+            StartCoroutine(StopMoveCam());
+        }
+
+        if (inPackageArea && !isActivated)
+        {
+            GameManager.instance.cam1.SetActive(false);
+            GameManager.instance.cam2.SetActive(false);
+            mainCam.gameObject.SetActive(false);
+            cam1.gameObject.SetActive(true);
+            cam2.gameObject.SetActive(false);
+            cam3.gameObject.SetActive(false);
+            cam4.gameObject.SetActive(false);
+            cam5.gameObject.SetActive(false);
+            cam6.gameObject.SetActive(false);
+
+            StartCoroutine(MoveToNextCamera1());
+            isActivated = true;
         }
     }
 
@@ -66,6 +149,18 @@ public class TutorialCamControl : MonoBehaviour
             yield return StartCoroutine(MoveCamera(targetPosition, transitionSpeed, mainCam));
             currentPositionIndex++;
         }
+    }
+
+    IEnumerator StopMoveCam()
+    {
+        yield return new WaitForSeconds(3f);
+        GameManager.instance.cam1.SetActive(true);
+        GameManager.instance.cam2.SetActive(true);
+        mainCam.gameObject.SetActive(false);
+
+        GameManager.instance.p1.isFreeze = false;
+        GameManager.instance.p2.isFreeze = false;
+
     }
 
     IEnumerator MoveCamera(Transform targetPos, float lerpSpeed, Camera cam)
@@ -83,6 +178,30 @@ public class TutorialCamControl : MonoBehaviour
                 mainCam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, targetPos.rotation, Time.deltaTime * lerpSpeed);
             }
 
+            yield return null;
+        }
+    }
+
+    #endregion
+
+    #region Package Area
+    IEnumerator MoveToNextCamera1()
+    {
+        while (currentPositionIndex1 < cameraPositions1.Length)
+        {
+            Transform targetPosition = cameraPositions1[currentPositionIndex1];
+            yield return StartCoroutine(MoveCamera1(targetPosition, transitionSpeed, cam1));
+            currentPositionIndex1++;
+        }
+    }
+
+
+    IEnumerator MoveCamera1(Transform targetPos, float lerpSpeed, Camera cam)
+    {
+        while (Vector3.Distance(cam1.transform.position, targetPos.position) > 0.1f)
+        {
+            mainCam.transform.position = Vector3.Lerp(cam.transform.position, targetPos.position, Time.deltaTime * lerpSpeed * 0.5f);
+            mainCam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, targetPos.rotation, Time.deltaTime * lerpSpeed * 0.5f);
             yield return null;
         }
     }
