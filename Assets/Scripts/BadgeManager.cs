4e19d9fb208d585231cae4a88035614a86b742ee
Chummy-Guy <78655854+Chummy-Guy@users.noreply.github.com>
2024-02-17 19:40:51 -0500

    gold badge selection

--- Assets/Scripts/BadgeManager.cs
+++ Assets/Scripts/BadgeManager.cs
@@ -13,15 +13,20 @@ public class BadgeManager : MonoBehaviour
     [SerializeField] private int goldBadgesPerLevel = 4;
     //the badges that can be earned in the current level
     private List<BadgeInfo> allSilverBadges;
-    private List<BadgeInfo> earnableSilverBadges;
+    [SerializeField] private List<BadgeInfo> earnableSilverBadges;
     private List<BadgeInfo> allGoldbadges;
-    private BadgeInfo[] earnableGoldBadges;
+    private List<BadgeInfo> availableGoldBadges;
+    [SerializeField] private BadgeInfo[] earnableGoldBadges;
 
 
     private void Start()
     {
         //set our maximum number of earnable gold badges per level
         earnableGoldBadges = new BadgeInfo[goldBadgesPerLevel];
+        allSilverBadges = new List<BadgeInfo>();
+        allGoldbadges = new List<BadgeInfo>();
+        earnableSilverBadges = new List<BadgeInfo>();
+        availableGoldBadges = new List<BadgeInfo>();
 
         foreach (var badge in allBadges)
         {
@@ -33,8 +38,12 @@ public class BadgeManager : MonoBehaviour
             {
                 //add to a list that we can choose from randomly
                 allGoldbadges.Add(badge);
+                availableGoldBadges.Add(badge);
             }
         }
+
+        //this is temporary, normally we want this to run on level start only
+        ChooseLevelBadges();
            
     }
     private void ChooseLevelBadges()
@@ -47,14 +56,22 @@ public class BadgeManager : MonoBehaviour
 
         foreach (var badge in allSilverBadges)
         {
+            //we can earn all silver badges per level
             earnableSilverBadges.Add(badge);
         }
 
         for (int i = 0; i < randomIndex.Length; i++)
         {
-
+            //get a random gold badge (a value between 0 and the number of gold badges)
+            //Do this for each earnable badge we have
+            int randomValue = Random.Range(0, availableGoldBadges.Count);
+            randomIndex[i] = randomValue;
+            //make our earnable badges set to the number of index in the available badges list
+            earnableGoldBadges[i] = availableGoldBadges[randomValue];
+            //take that badge out of the badge pool
+            availableGoldBadges.RemoveAt(randomValue);
         }
         
-
+        
     }
 }
