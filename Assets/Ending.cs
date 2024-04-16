1ea674dcac3ea1f0ef1b5210df2f5bdf5c6aeca5
KittyC513 <82276776+KittyC513@users.noreply.github.com>
2024-04-16 11:39:47 -0400

    Implement End Comic in Level2

--- Assets/Ending.cs
+++ Assets/Ending.cs
@@ -8,11 +8,24 @@ public class Ending : MonoBehaviour
     [SerializeField]
     private GameObject canvas;
     private BadgeManager badgeManager;
+    [SerializeField]
+    private GameObject comicCanvas;
+    [SerializeField]
+    private Animator comicAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         badgeManager = FindAnyObjectByType<BadgeManager>();
+
+        if (comicCanvas != null)
+        {
+            comicAnim = comicCanvas.GetComponent<Animator>();
+            if (comicAnim != null)
+            {
+                comicCanvas.SetActive(false);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -87,17 +100,43 @@ public class Ending : MonoBehaviour
                 canvas.gameObject.SetActive(true);
             }
 
+            GameManager.instance.p1.isFreeze = true;
+            GameManager.instance.p2.isFreeze = true;
+
+
+            //if (badgeManager != null)
+            //{
+            //    badgeManager.RunFinalCheck();
+            //}
+
+            StartCoroutine(ComicStart());
+
+        }
+    }
+
+    IEnumerator ComicStart()
+    {
+        comicCanvas.SetActive(true);
+        comicAnim.SetTrigger("Level2");
+        yield return new WaitForSeconds(26);
+        GameManager.instance.p1.isFreeze = false;
+        GameManager.instance.p2.isFreeze = false;
+
+        GameManager.instance.changeSceneTimes += 1;
+        GameManager.instance.WertherRequestWasCompleted = true;
+        Loader.Load(Loader.Scene.ScoreCards);
+    }
+
+    void SkipComic()
+    {
+        if (GameManager.instance.p1.ReadSkipButton() || GameManager.instance.p2.ReadSkipButton())
+        {
+            StopCoroutine(ComicStart());
+            comicCanvas.SetActive(false);
+            GameManager.instance.UnfreezePlayer();
             GameManager.instance.changeSceneTimes += 1;
             GameManager.instance.WertherRequestWasCompleted = true;
-
-            if (badgeManager != null)
-            {
-                badgeManager.RunFinalCheck();
-            }
-
-
             Loader.Load(Loader.Scene.ScoreCards);
-
         }
     }
 
